a5fa5bf7c370b5ab90d2b5229682776432269fc3
Katie <157065653+Trenchcoat7@users.noreply.github.com>
2025-02-10 11:01:41 -0500

    made 2 branches

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -9,8 +9,8 @@
  * and reacts by incrementing the display. This is done in the usual clock
  * fashion: the hour increments when the minutes roll over to zero.
  * 
- * @author Michael Kölling and David J. Barnes
- * @version 2016.02.29
+ * @author Katie Strong
+ * @version 2025.2.10
  */
 public class ClockDisplay
 {
