6d1907c28957155c4bd586d17ec66a357bb90b33
Katie <157065653+Trenchcoat7@users.noreply.github.com>
2025-02-18 18:04:17 -0500

    copied code from 24 hr internal clock to change the numbers

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -93,7 +93,33 @@ public class ClockDisplay
         if (hours.getValue() >= 13) 
         {
         
-            hours.setTime = hours.setTime - 12;            
+            int currentHour = hours.getValue();
+        
+        if (currentHour > 12){
+        
+                currentHour = currentHour -12;
+        
+        }
+        
+        if(currentHour == 0){
+        
+            currentHour = 12;
+        
+        }
+        
+        displayString = currentHour + ":" + 
+                        minutes.getDisplayValue();
+                        
+        if(hours.getValue() >= 12){
+        
+            displayString = displayString + "pm";
+        
+        }
+        else{
+        
+            displayString = displayString + "am";
+            
+        }            
         
         }
     }
